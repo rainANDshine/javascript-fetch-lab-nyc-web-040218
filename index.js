a9948b6e284640277946ddd3a09f904f90982b47
Shun Yao <ysmiracle@hotmail.com>
2018-05-20 09:12:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,9 +14,8 @@ function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab';
   const token = '065d3b08c94396a8eddfa50375264d4eee4253e0';
 
-  fetch('https://api.github.com/repos/learn-co-students/javascript-fetch-lab-nyc-web-040218/forks').then(resp => resp.json()).then(json => console.log(json));
-
-  
+  fetch('https://api.github.com/repos/learn-co-students/javascript-fetch-lab-nyc-web-040218/forks', {headers: {Authorization: `token ${token}`}}
+  ).then(resp => resp.json()).then(json => console.log(json));
 }
 
 function getToken() {
